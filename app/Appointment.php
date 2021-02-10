9f922c3f472d5403b219f119368309ed74657c2f
caun94 <cristobalugarte6@gmail.com>
2021-02-10 15:06:52 -0300

    add profesional

--- app/Appointment.php
+++ app/Appointment.php
@@ -16,7 +16,7 @@ class Appointment extends Model
     public static function tomorrow_appoiments()
     {
     	$tomorrow = Carbon::tomorrow();
-        return DB::select( DB::raw("select Profesional , Estado,Nombre_paciente,Apellidos_paciente,Celular,Hora_inicio,TotalAtencion,Mail from appointments as a join treatments
+        return DB::select( DB::raw("select a.Profesional , Estado,Nombre_paciente,Apellidos_paciente,Celular,Hora_inicio,TotalAtencion,Mail from appointments as a join treatments
         on a.Tratamiento_Nr = treatments.Atencion where  a.id in (SELECT max(id) FROM appointments where Fecha = '".$tomorrow."'  group by Tratamiento_Nr) and Estado in ('No Confirmado','Agenda Online') order by Hora_inicio asc") );
     }
 
