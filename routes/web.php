2f48252d9978f192a6a162977dc68958ec8986b5
caun94 <cristobalugarte6@gmail.com>
2021-07-22 13:04:04 -0400

    Jue 22 Jul New Iver and Cristi

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
-Route::get('/pdf/{id}', 'PDFController@download')->name('permiso');;
+Route::get('/pdf/{id}', 'PDFController@download')->name('permiso');
 
 Route::get('/you-wsp', 'HomeController@panel')->name('you-wsp');
 Route::get('/excel', 'HomeController@excel')->name('excel');
