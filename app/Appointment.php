6a07b05693a7849a3b4bb6908e09327dc91bb68c
caun94 <cristobalugarte6@gmail.com>
2021-07-30 10:44:45 -0400

    Vie 30 Jul New Wsp

--- app/Appointment.php
+++ app/Appointment.php
@@ -21,9 +21,19 @@ class Appointment extends Model
 
     public static function tomorrow_appoiments()
     {
-    	$tomorrow = Carbon::tomorrow();
+        $tomorrow = Carbon::tomorrow();
+        if ($tomorrow->format('l') == 'Saturday'){
+            $monday = $tomorrow->copy()->addDays(2);
+            return DB::select( DB::raw("select a.id,a.Profesional , a.Tratamiento_Nr, Estado,Nombre_paciente,Apellidos_paciente,Celular,Hora_inicio,TotalAtencion+Avance as TotalAtencion,Mail from appointments as a join treatments
+        on a.Tratamiento_Nr = treatments.Atencion where  a.id in (SELECT max(id) FROM appointments where Fecha = '".$tomorrow."' or Fecha ='".$monday."'  group by Tratamiento_Nr) and Estado in ('No Confirmado','Agenda Online') order by Fecha asc,Hora_inicio asc") );
+        }
+        if ($tomorrow->format('l') == 'Sunday'){
+            $monday = $tomorrow->copy()->addDays(1);
+            return DB::select( DB::raw("select a.id,a.Profesional , a.Tratamiento_Nr, Estado,Nombre_paciente,Apellidos_paciente,Celular,Hora_inicio,TotalAtencion+Avance as TotalAtencion,Mail from appointments as a join treatments
+        on a.Tratamiento_Nr = treatments.Atencion where  a.id in (SELECT max(id) FROM appointments where Fecha = '".$tomorrow."' or Fecha ='".$monday."'  group by Tratamiento_Nr) and Estado in ('No Confirmado','Agenda Online') order by Fecha asc,Hora_inicio asc") );
+        }
         return DB::select( DB::raw("select a.id,a.Profesional , a.Tratamiento_Nr, Estado,Nombre_paciente,Apellidos_paciente,Celular,Hora_inicio,TotalAtencion+Avance as TotalAtencion,Mail from appointments as a join treatments
-        on a.Tratamiento_Nr = treatments.Atencion where  a.id in (SELECT max(id) FROM appointments where Fecha = '".$tomorrow."'  group by Tratamiento_Nr) and Estado in ('No Confirmado','Agenda Online') order by Hora_inicio asc") );
+        on a.Tratamiento_Nr = treatments.Atencion where  a.id in (SELECT max(id) FROM appointments where Fecha = '".$tomorrow."'  group by Tratamiento_Nr) and Estado in ('No Confirmado','Agenda Online') order by Fecha asc,Hora_inicio asc") );
     }
 
     public static function tomorrow_appoiment($nr){
